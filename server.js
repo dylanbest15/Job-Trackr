774f7009c42d483ee0ada69c956c1be8614824fb
Dylan Best <best.dylan15@gmail.com>
2020-11-11 20:53:22 -0500

    passport config and routes

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const session = require("express-session");
+const compression = require("compression");
 
-// const routes = require("./routes");
+// require passport
+const passport = require("./config/passport");
+
+const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -8,17 +13,21 @@ const PORT = process.env.PORT || 3001;
 const db = require("./models");
 
 // Define middleware here
+app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-// Add routes, both API and view
-// app.use(routes);
 
-// Connect to the Mongo DB
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+// Add routes, both API and view
+app.use(routes);
 
 // Start the API server
 db.sequelize.sync().then(() => {
