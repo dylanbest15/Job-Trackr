4dd81aa17566a911a275e1308cc96dda9eabaeff
Alissa Champigny <achampigny4@gmail.com>
2020-11-19 08:51:43 -0500

    removed pending route, added benchmark route

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,10 @@ import Login from "./pages/Login/Login";
 import Search from "./pages/Search/Search";
 import Register from "./pages/Register/Register";
 import Applied from "./pages/Applied/Applied";
-import Pending from "./pages/Pending/Pending";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Articles from "./pages/Articles/Articles";
+// import Benchmark from "./pages/Benchmarks/Benchmarks";
 
 
 function App() {
@@ -19,8 +19,8 @@ function App() {
             <Route path="/register" component={Register} />
             <Route path="/dashboard" component={Dashboard} />
             <Route path="/search" component={Search} />
-            <Route path="/pending" component={Pending} />
             <Route path="/applied" component={Applied} />
+            {/* <Route path="/benchmarks" component={Benchmark} /> */}
             <Route path="/resources" component={Articles} />
             {/* <Route path="/suggestedJobs" component={SuggestedJobs} /> */}
             {/* <Route path="/profile" component={Profile} /> */}
