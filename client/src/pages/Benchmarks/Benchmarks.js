51550d3efabb58fd6ac3a0700bc1e90403f23c7b
Brian OLeary <oleary.brian1@gmail.com>
2020-11-21 13:50:09 -0500

    api benchmark call

--- client/src/pages/Benchmarks/Benchmarks.js
+++ client/src/pages/Benchmarks/Benchmarks.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import BenchmarkCard from "./BenchmarkCard";
+import API from '../../utils/BenchmarkAPI';
 
 // import benchmarks API
 
@@ -11,17 +12,23 @@ function Benchmarks() {
   useEffect(() => {
     // get benchmarks
     // set benchmarks to benchmarks array
-
+    loadBenchmarks();
     // get user benchmarks
     // set user benchmarks to user benchmark array
   })
-
+  function loadBenchmarks() {
+    API.getBenchmarks()
+      .then(res =>
+        setBenchmarks(res.data + console.log(res.data))
+      )
+      .catch(err => console.log(err));
+  };
   return (
     <>
-<h1>Benchmarks</h1>
-    {/* loop through benchmark card component with props from benchmarks and user benchmarks states */}
-    <BenchmarkCard
-                            />
+      <h1>Benchmarks</h1>
+      {/* loop through benchmark card component with props from benchmarks and user benchmarks states */}
+      <BenchmarkCard
+      />
     </>
   )
 }
