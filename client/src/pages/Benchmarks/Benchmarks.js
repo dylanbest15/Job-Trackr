76df9654be060f249b8bbb7bccefea1eb68a6337
Brian OLeary <oleary.brian1@gmail.com>
2020-11-21 10:26:02 -0500

    benchmarks

--- client/src/pages/Benchmarks/Benchmarks.js
+++ client/src/pages/Benchmarks/Benchmarks.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import BenchmarkCard from "./BenchmarkCard";
+
 // import benchmarks API
 
 function Benchmarks() {
@@ -16,9 +18,11 @@ function Benchmarks() {
 
   return (
     <>
-
+<h1>Benchmarks</h1>
     {/* loop through benchmark card component with props from benchmarks and user benchmarks states */}
-
+    <BenchmarkCard
+                            />
     </>
   )
 }
+export default Benchmarks;
--- models/Benchmark.js
+++ models/Benchmark.js
@@ -18,7 +18,7 @@ module.exports = function(sequelize, DataTypes) {
     description: {
       type: DataTypes.TEXT,
       allowNull: false
-    }
+    },
   });
 
   return Benchmark;
