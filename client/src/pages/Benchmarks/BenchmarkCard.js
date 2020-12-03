c4d6a5141dcd80e163b9cebca8698f1ba699cf50
Brian OLeary <oleary.brian1@gmail.com>
2020-12-02 19:47:54 -0500

    benchmark card

--- client/src/pages/Benchmarks/BenchmarkCard.js
+++ client/src/pages/Benchmarks/BenchmarkCard.js
@@ -40,27 +40,27 @@ LinearProgressWithLabel.propTypes = {
 let cardBackgroundColor = "white"
 
 const useStyles = makeStyles({
-    root: {
-        maxWidth: 275,
-        margin: 15,
-        height: 200,
-        backgroundColor: "LightGray",
-      },
-      gold: {
-        maxWidth: 275,
-        backgroundColor: "#FFCC66",
-      },
-      bullet: {
-        display: 'inline-block',
-        margin: '0 2px',
-        transform: 'scale(0.8)',
-      },
-      title: {
-        fontSize: 14,
-      },
-      pos: {
-        marginBottom: 12,
-      },
+  root: {
+    maxWidth: 275,
+    margin: 15,
+    height: 200,
+    backgroundColor: "LightGray",
+  },
+  gold: {
+    maxWidth: 275,
+    backgroundColor: "#FFCC66",
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
 });
 
 
@@ -68,8 +68,7 @@ const useStyles = makeStyles({
 function BenchmarkCard({ originalBenchmarkInfo, userInfo }) {
   const classes = useStyles();
   const [progress, setProgress] = React.useState(0);
-  const 
-
+  const goldVariable = gold;
   React.useEffect(() => {
     console.log(userInfo)
     updateProgress();
@@ -84,7 +83,7 @@ function BenchmarkCard({ originalBenchmarkInfo, userInfo }) {
       else { (oldProgress = ((stat / originalBenchmarkInfo.value) * 100)) }
     };
     if (originalBenchmarkInfo.type == "Application") { progressMath(userInfo.jobs_applied) }
-    else if (originalBenchmarkInfo.type =="Offer") { progressMath(userInfo.jobs_offered) }
+    else if (originalBenchmarkInfo.type == "Offer") { progressMath(userInfo.jobs_offered) }
     else if (originalBenchmarkInfo.type == "Acceptance") { progressMath(userInfo.jobs_accepted) }
     else if (originalBenchmarkInfo.type == "Interview") { progressMath(userInfo.jobs_interviewed) }
     else if (originalBenchmarkInfo.type == "Letter") { progressMath(userInfo.jobs_lettersent) }
@@ -107,12 +106,12 @@ function BenchmarkCard({ originalBenchmarkInfo, userInfo }) {
         <Typography variant="body2" component="p">
           {originalBenchmarkInfo.description}
 
-          </Typography>
-          <div className={classes.root}>
-                    <LinearProgressWithLabel value={progress} />
-                </div>
-        </CardContent>
-      </Card>
+        </Typography>
+        <div className={classes.root}>
+          <LinearProgressWithLabel value={progress} />
+        </div>
+      </CardContent>
+      </Card >
 
   )
 }
