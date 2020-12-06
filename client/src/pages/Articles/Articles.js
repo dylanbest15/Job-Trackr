0db466472757cbee49122365f49ef5d807063324
melaniede <debarrosmelanie@gmail.com>
2020-12-06 18:45:00 -0500

    Add header to Articles Page

--- client/src/pages/Articles/Articles.js
+++ client/src/pages/Articles/Articles.js
@@ -3,14 +3,23 @@ import ArticleGrid from "../../components/ArticleGrid/index"
 import ResponsiveDrawer from "../../components/SideBar/SideBar";
 import { makeStyles } from '@material-ui/core/styles';
 import "./style.css";
-import SimpleTabs from '../../components/Header/Header';
+import SimpleTabs from './../../components/Header/Header';
 // import SimpleTabs from '../Header/Header';
 
+const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
   },
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  }
 }));
 
 function Articles() {
@@ -19,8 +28,13 @@ function Articles() {
   return (
     <div className={classes.root}>
       <ResponsiveDrawer />
-      <SimpleTabs />
-        <ArticleGrid />
+        <main className={classes.content}>
+         <div className={classes.toolbar} />
+           <div className="container">
+            <SimpleTabs />
+            <ArticleGrid />
+          </div>
+        </main>
     </div>
   );
 }
